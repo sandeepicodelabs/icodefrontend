335f214edc6937da3c7b145ee2a4f6b8df2d6d5d
sandeepicodelabs <sandeep.bishnoi@icodelabs.co>
2023-06-29 22:52:15 +0530

    updated code for landing page

---
+++ src/components/button/index.js
@@ -0,0 +1,20 @@
+import React from "react"
+import './style.css';
+
+const ButtonBox = (props) => {
+  const { type, buttonname, className } = props;
+  return (
+    <>
+      <div className="button-box">
+        <button
+          type={type}
+          className={className}
+        >
+          {buttonname}
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default ButtonBox;
