582587eaba5f864dbd4d7adc0fcb4a532ccc2e94
sandeepicodelabs <sandeep.bishnoi@icodelabs.co>
2023-06-29 19:57:40 +0530

    lendingpageimage

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,21 +11,40 @@ require("dotenv").config({
 }); 
 
 const strapiConfig = { 
-  apiURL: process.env.STRAPI_API_URL,
-  accessToken: process.env.STRAPI_TOKEN,
+  apiURL:"http://localhost:1337",
+  accessToken: process.env.STRAPI_TOKEN, 
   collectionTypes: [
-   
+    "book",
+    "product",
+    "article",
+    "company-logo"
   ], 
-
+  queryLimit: 1000, 
 }; 
 
 
 module.exports = { 
-  plugins: [
+  plugins: [ 
+    "gatsby-plugin-image",
+    "gatsby-transformer-sharp",
     {
-      resolve: `gatsby-source-strapi`,
-      options: strapiConfig,
-    },
+      resolve: "gatsby-source-strapi",
+      options: {
+        ...strapiConfig
+      },
+    },  
+     
+      // { 
+      //   resolve: `gatsby-source-filesystem`,
+      //   options: {
+      //     // The unique name for each instance
+      //     name: `images`,
+      //     // Path to the directory
+      //     path: `${__dirname}/src/assets/images`,
+      //   },
+      // }, 
+     
   ],
   
 };
+ 
