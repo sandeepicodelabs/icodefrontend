1fe1379e744366756aeb894437d3fb5bc2f18801
Saroj kumari <sarojkumari0290@gmail.com>
2023-08-07 21:25:02 +0530

    project list page completed.

--- src/components/Cards/projectcard/index.js
+++ src/components/Cards/projectcard/index.js
@@ -1,28 +1,36 @@
-import React from 'react';
-import linkIcon from '../../../assets/images/link-icon.png'
-import './style.css'
- 
+import React from "react";
+import linkIcon from "../../../assets/images/link-icon.png";
+import "./style.scss";
 
 export default function ProjectListCard(props) {
-    const { img, boxList } = props;
+  const { img, boxList } = props;
   return (
     <>
-    <div className="project-box">
-      <img src={img} alt='st logo' />
-      <div className="project-body-box">
-        <h3>Smart hire
-          <span>
-            <a href="#"><img src={linkIcon} alt=' ' /></a>
-          </span>
-        </h3>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
-        <div className="poroject-box-list">
-          {boxList?.map((i) => (
-            <div className="box-list" key={i}>{i}</div>
-          ))}
+      <div className="project-box">
+        <div className="project-img">
+          <img src={img} alt="st logo" />
+        </div>
+        <div className="project-body-box">
+          <h3>
+            Smart hire
+            <a href="#">
+              <img src={linkIcon} alt=" " />
+            </a>
+          </h3>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s
+          </p>
+          <div className="poroject-box-list">
+            {boxList?.map((i) => (
+              <div className="box-list" key={i}>
+                <span>{i}</span>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
-  </>
-  )
+    </>
+  );
 }
--- src/pages/projectlist/index.js
+++ src/pages/projectlist/index.js
@@ -5,7 +5,8 @@ import EstimateCard from "../../components/Cards/estimatecard";
 import Footer from "../../components/Footer/Footer";
 import ProjectListCard from "../../components/Cards/projectcard";
 import { graphql } from "gatsby";
-import "../../assets/css/custom.css";
+import "../projectList.scss";
+// import "../../assets/css/custom.css";
 
 export default function ProjectList({ data }) {
   const projectlist = data?.allStrapiProjectList?.edges;
@@ -15,27 +16,32 @@ export default function ProjectList({ data }) {
     <div className="project-list-page">
       <Header />
       <div className="project-wrap-box">
-        <section>
-          <div className="project-list-cover">
+        <section className="cover-background">
+          <div className="contentWidth">
             <h1>
               Successfully developed and delivered 300+ web and mobile
               application
             </h1>
-            <h1>{projectlist && projectlist?.node?.Heading}</h1>
-            <p>Select your marketplace type</p>
-            <div className="hiring-buttons">
-              <ButtonBox buttonname="Events and experiences" />
-              <ButtonBox
-                buttonname="E-Commerce & retail"
-                className="hire-dev-button"
-              />
-              <ButtonBox buttonname="Rentals" className="hire-dev-button" />
-              <ButtonBox buttonname="Services" className="hire-dev-button" />
+            <div className="selectCategory">
+              <h4>{projectlist && projectlist?.node?.Heading}</h4>
+              <p>Select your marketplace type</p>
+              <div className="hiring-buttons">
+                <ButtonBox
+                  buttonname="Events and experiences"
+                  className="hire-dev-button"
+                />
+                <ButtonBox
+                  buttonname="E-Commerce & retail"
+                  className="hire-dev-button"
+                />
+                <ButtonBox buttonname="Rentals" className="hire-dev-button" />
+                <ButtonBox buttonname="Services" className="hire-dev-button" />
+              </div>
             </div>
           </div>
         </section>
-        <section>
-          <div className="projectlist-main-box">
+        <section className="projectlist-main-box">
+          <div className="contentWidth ">
             <div className="project-wrap">
               {projectlist?.map((item, i) => (
                 <ProjectListCard
@@ -48,9 +54,11 @@ export default function ProjectList({ data }) {
             {/* <PaginationBox /> */}
           </div>
         </section>
-        <section className="estimate-section-wrap">
-          <EstimateCard />
-        </section>
+        <div className="contentWidth">
+          <section className="estimate-section-wrap">
+            <EstimateCard />
+          </section>
+        </div>
       </div>
       <Footer />
     </div>
