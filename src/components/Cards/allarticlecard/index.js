d2b7fca78112f8716de686fab173f2d336a9acba
Saroj kumari <sarojkumari0290@gmail.com>
2023-08-08 18:55:48 +0530

    blog changes

--- src/components/Cards/allarticlecard/index.js
+++ src/components/Cards/allarticlecard/index.js
@@ -1,32 +1,40 @@
-import React from 'react'
-import './style.css'
+import React from "react";
+import "./allarticlecard.scss";
 
 export default function AllArticleCard(props) {
-    const { articleTitle, articledescription, postedname, postdate, cardtitle, img, cardprofile } = props;
-    //console.log("article image",img)
+  const {
+    articleTitle,
+    articledescription,
+    postedname,
+    postdate,
+    cardtitle,
+    img,
+    cardprofile,
+  } = props;
+  //console.log("article image",img)
   return (
     <>
-    <div className="all-article-card-box">
-      <div className="all-article-cover-image">
-        <img src={img} alt="St Logo" />
-      </div>
-      <div className="all-article-card-data">
-        <h5 className="all-article-card-title">{articleTitle}</h5>
-        <div className="all-article-card-detail-box">
-          <div className="all-article-profile-list">
-            <img src={cardprofile}  alt='st logo'/>
-            <div className="all-article-profile-wrap">
-              <div className="all-article-name"><span>Posted by :</span> {postedname}</div>
-              <div className="all-article-post-date">{postdate}</div>
+      <div className="all-article-card-box">
+        <div className="all-article-cover-image">
+          <img src={img} alt="St Logo" />
+        </div>
+        <div className="all-article-card-data">
+          <h5 className="all-article-card-title">{articleTitle}</h5>
+          <div className="all-article-card-detail-box">
+            <div className="all-article-profile-list">
+              <img src={cardprofile} alt="st logo" />
+              <div className="all-article-profile-wrap">
+                <div className="all-article-name">
+                  <span>Posted by :</span> {postedname}
+                </div>
+                <div className="all-article-post-date">{postdate}</div>
+              </div>
             </div>
+            <div className="all-article-card-description">{cardtitle}</div>
           </div>
-          <div className="all-article-card-description">{cardtitle}</div>
-        </div>
-        <div className="all-article-description">
-          {articledescription}
+          <div className="all-article-description">{articledescription}</div>
         </div>
       </div>
-    </div>
-  </>
-  )
+    </>
+  );
 }
