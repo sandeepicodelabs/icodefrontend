7127ebfc9a7ac4e2a5e70fe68f74679eeb8162e7
Saroj kumari <sarojkumari0290@gmail.com>
2023-08-10 21:40:23 +0530

    technologies page completed.

--- src/components/Cards/StackToolsCard/index.js
+++ src/components/Cards/StackToolsCard/index.js
@@ -1,30 +1,23 @@
-import React from 'react'
-import './style.css'
+import React from "react";
+import "../../../pages/StackToolsCard.scss";
 
-const StackToolsCard=(props)=> {
-    const {name,techchild } = props;
-   // console.log('techchild', techchild)
+const StackToolsCard = (props) => {
+  const { name, techchild } = props;
 
   return (
-    <>
     <div className="stack-card">
-  <div className="stack-card-box">
-      <div className="stack-label">{name}</div>
-    <div className="stack-card-tech">
-    <ul>
-                    {techchild?.map((i) => (
-                        <li
-                            key={i}
-                        > 
-                            {i.stacktech}
-                        </li>
-                    ))}
-                </ul>
-    </div>  
-  </div>
-</div>
-    </>
-  )
-}
+      <div className="stack-card-box">
+        <div className="stack-label">{name}</div>
+        <div className="stack-card-tech">
+          <ul>
+            {techchild?.map((i) => (
+              <li key={i}>{i.stacktech}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default StackToolsCard
+export default StackToolsCard;
