00c51d3e9e823ba5384f6960ecfcfa0891e73653
Saroj kumari <sarojkumari0290@gmail.com>
2023-08-24 18:53:43 +0530

    seo

--- src/components/Seo/seo.js
+++ src/components/Seo/seo.js
@@ -1,29 +1,22 @@
-import React from "react";
-import { useSiteMetadata } from "../../hooks/use-site-metadata";
+import React from "react"
+import { useSiteMetadata } from "../../hooks/use-site-metadata"
 
-export const SEO = ({ title, description, pathname, children, keyword }) => {
-  const {
-    title: defaultTitle,
-    description: defaultDescription,
-    siteUrl,
-    keyword: defaultkeyword,
-  } = useSiteMetadata();
-
-  const seo = {
-    title: title || defaultTitle,
-    description: description || defaultDescription,
-    keyword: keyword || defaultkeyword,
-    url: `${siteUrl}${pathname || ``}`,
-  };
-
-  return (
-    <>
-      <title>{seo.title}</title>
-      <meta name="description" content={seo.description} />
-      <meta name="keyword" content={seo.keyword} />
-      <meta name="image" content={seo.image} />
-
-      {children}
-    </>
-  );
-};
+export const SEO = ({ title, description, pathname, children }) => {
+    const { title: defaultTitle, description: defaultDescription, siteUrl} = useSiteMetadata()
+  
+    const seo = {
+      title: title || defaultTitle,
+      description: description || defaultDescription,
+      url: `${siteUrl}${pathname || ``}`,
+    }
+  
+    return (
+      <>
+        <title>{seo.title}</title>
+        <meta name="description" content={seo.description}/>
+        <meta name="image" content={seo.image} />
+        
+        {children}
+      </>
+    )
+  }
