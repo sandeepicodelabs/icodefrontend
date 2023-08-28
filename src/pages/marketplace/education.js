eb99556170c7f03d33c90bfe5ece5cb89566bcc8
sandeepicodelabs <sandeep.bishnoi@icodelabs.co>
2023-08-28 19:29:44 +0530

    edu

---
+++ src/pages/marketplace/education.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ButtonBox from "../../components/button";
+
+export default function education() {
+  return (
+     <>
+         <section className="marketplace-header">
+        <div className="contentWidth">
+          <p>Education App Development Services</p>
+          <h1>
+          A leading education software development company redefining learning for 10+ millions students and trainers
+          </h1>
+          <ButtonBox buttonname="Share your app idea" className="btn" />
+        </div>
+      </section>
+     </>
+  )
+}
